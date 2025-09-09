e23d7560ec4a7ba3216541657c5736be7b0b46ad
MonaFP <info@fruehchen-portal.de>
2025-09-09 19:24:19 +0200

    entfernt

--- dist-electron/preload.js
+++
@@ -1,9 +0,0 @@
-// electron/preload.ts
-import { contextBridge, ipcRenderer } from "electron";
-contextBridge.exposeInMainWorld("rawalite", {
-  db: {
-    load: () => ipcRenderer.invoke("db:load"),
-    save: (data) => ipcRenderer.invoke("db:save", data)
-  }
-});
-//# sourceMappingURL=preload.js.map
