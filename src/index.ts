7e31f90714a1ec025044fefba67c864ac37b68f1
MonaFP <info@fruehchen-portal.de>
2025-09-10 07:28:58 +0200

    Update XYZ

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-export * from './entities/Kunde.ts';
-export * from './entities/Rechnung.ts';
- // ... Paket, Angebot
-export * from './services/NummernkreisService.ts';
-export * from './valueObjects/Nummernkreis.ts';
+export * from './entities/Kunde';
+export * from './services/NummernkreisService';
+
+// Page exports
+export { default as AngebotDetailPage } from './pages/AngebotDetailPage';
+export { default as AngebotePage } from './pages/AngebotePage';
+export { default as DashboardPage } from './pages/DashboardPage';
+export { default as EinstellungenPage } from './pages/EinstellungenPage';
+export { default as KundenPage } from './pages/KundenPage';
+export { default as NotFoundPage } from './pages/NotFoundPage';
+export { default as PaketePage } from './pages/PaketePage';
+export { default as RechnungenPage } from './pages/RechnungenPage';
